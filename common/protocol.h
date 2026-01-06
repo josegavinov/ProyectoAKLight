a72a24a5ede32b676c594a3e69d1309943e3b6fa
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2026-01-05 23:07:00 -0500

    Creación de estructura del proyecto y protocol.h

---
+++ common/protocol.h
@@ -0,0 +1,13 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define MAX_TOPIC 128
+#define MAX_MESSAGE 256
+#define BUFFER_SIZE 512
+
+// Tipos de mensajes
+#define CMD_PUBLISH   "PUBLISH"
+#define CMD_SUBSCRIBE "SUBSCRIBE"
+#define CMD_MESSAGE   "MESSAGE"
+
+#endif
